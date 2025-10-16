1cfd1cd324560814055ac513f087826489fd02f8
dzi-niska <dzhamarko@gmail.com>
2025-10-16 14:05:04 -0500

    Update Main.java

--- src/main/java/com/plurasight/Main.java
+++ src/main/java/com/plurasight/Main.java
@@ -1,4 +1,9 @@
-package com.plurasight;
+package org.example;
+
+import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Screens.homeScreen();
+    }
 }
